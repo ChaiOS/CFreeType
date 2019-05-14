a2a28907bf68a5b86ef856c74152dcd1b7336948
Abdurrahman Avci <abdurrahmanavci@gmail.com>
2019-05-14 22:57:10 +0300

    Switch to swift tools version 4.2.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "CFreeType",
-    pkgConfig: "freetype2",
-    providers: [
-        .apt(["libfreetype6-dev"]),
-        .brew(["freetype2"])
-    ],
     products: [
         .library(name: "CFreeType", targets: ["CFreeType"])
     ],
     targets: [
-        .target(name: "CFreeType")
+        .systemLibrary(
+            name: "CFreeType",
+            pkgConfig: "freetype2",
+            providers: [
+                .apt(["libfreetype6-dev"]),
+                .brew(["freetype2"])
+            ]
+        )
     ]
 )
